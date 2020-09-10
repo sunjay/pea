579478ae8855ae8c29f648d065c250ed01798c83
Sunjay Varma <varma.sunjay@gmail.com>
2020-09-10 13:38:22 -0600

    Added structopt and reading program from command line arguments

--- src/bin/pea.rs
+++ src/bin/pea.rs
@@ -1,11 +1,13 @@
 #![deny(unused_must_use)]
 
 use std::process;
-use std::path::Path;
+use std::path::PathBuf;
 use std::sync::Arc;
+use std::str::FromStr;
 
 use parking_lot::RwLock;
 use termcolor::ColorChoice;
+use structopt::StructOpt;
 
 use pea::{
     diagnostics::Diagnostics,
@@ -14,6 +16,54 @@ use pea::{
     codegen,
     interpreter::Status,
 };
+
+/// A command line argument that configures the coloring of the output
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ColorArg(pub ColorChoice);
+
+impl Default for ColorArg {
+    fn default() -> Self {
+        ColorArg(ColorChoice::Auto)
+    }
+}
+
+impl ColorArg {
+    /// Allowed values the argument
+    pub const VARIANTS: &'static [&'static str] = &["auto", "always", "ansi", "never"];
+}
+
+impl FromStr for ColorArg {
+    type Err = &'static str;
+
+    fn from_str(src: &str) -> Result<ColorArg, &'static str> {
+        match src {
+            _ if src.eq_ignore_ascii_case("auto") => Ok(ColorArg(ColorChoice::Auto)),
+            _ if src.eq_ignore_ascii_case("always") => Ok(ColorArg(ColorChoice::Always)),
+            _ if src.eq_ignore_ascii_case("ansi") => Ok(ColorArg(ColorChoice::AlwaysAnsi)),
+            _ if src.eq_ignore_ascii_case("never") => Ok(ColorArg(ColorChoice::Never)),
+            _ => Err("valid values: auto, always, ansi, never"),
+        }
+    }
+}
+
+impl Into<ColorChoice> for ColorArg {
+    fn into(self) -> ColorChoice {
+        self.0
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "pea", about)]
+struct Options {
+    /// The program to compile and run
+    #[structopt(name = "input", parse(from_os_str))]
+    program_path: PathBuf,
+
+    /// Configure coloring of output
+    #[structopt(long = "color", parse(try_from_str), default_value = "auto",
+        possible_values = ColorArg::VARIANTS, case_insensitive = true)]
+    pub color: ColorArg,
+}
 
 macro_rules! quit {
     ($diag:expr, $($args:tt)*) => {
@@ -36,11 +86,10 @@ macro_rules! check_errors {
 }
 
 fn main() {
-    let program_path = Path::new("samples/run-pass/print_num.pea");
-    let color_choice = ColorChoice::Always;
+    let Options {program_path, color} = Options::from_args();
 
     let source_files = Arc::new(RwLock::new(SourceFiles::default()));
-    let diag = Diagnostics::new(source_files.clone(), color_choice);
+    let diag = Diagnostics::new(source_files.clone(), color.into());
 
     // Need this separate statement so we don't hold the write() lock in the
     // error case and end up with a deadlock
