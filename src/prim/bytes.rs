50a7da893eb7d4e35711f9352a9ae46fd62fb1ab
Sunjay Varma <varma.sunjay@gmail.com>
2020-09-17 15:38:41 -0600

    Redefined Bytes as a GC allocated dynamically-sized array of bytes

--- src/prim/bytes.rs
+++ src/prim/bytes.rs
@@ -1,15 +1,27 @@
 use std::fmt;
 
-use crate::gc;
+use crate::gc::{self, Gc};
 
 /// A growable ASCII byte string
 #[derive(Debug)]
-pub struct Bytes(pub Vec<u8>);
+pub struct Bytes(Gc<[u8]>);
+
+impl Default for Bytes {
+    fn default() -> Self {
+        Bytes(Gc::from(&[] as &[_]))
+    }
+}
+
+impl From<&[u8]> for Bytes {
+    fn from(value: &[u8]) -> Self {
+        Bytes(Gc::from(value))
+    }
+}
 
 impl fmt::Display for Bytes {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // write out each byte as if it was ascii
-        for &byte in &self.0 {
+        for &byte in &*self.0 {
             write!(f, "{}", byte as char)?;
         }
         Ok(())
@@ -18,6 +30,7 @@ impl fmt::Display for Bytes {
 
 impl gc::Trace for Bytes {
     fn trace(&self) {
-        let Bytes(_bytes) = self;
+        let Bytes(bytes) = self;
+        bytes.trace();
     }
 }
